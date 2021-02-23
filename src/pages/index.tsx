ce83ff8ac9968ae69066bb2f546e5bbec81c10a7
Carlos <carlosaugustocf99@gmail.com>
2021-02-23 20:48:04 -0300

    Project developer-in-the-go moved to developer-in-the-go-nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import { ExperienceBar } from "../components/Experiencebar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      
+      <ExperienceBar/>
+      
+    </div>
+  )
+}
