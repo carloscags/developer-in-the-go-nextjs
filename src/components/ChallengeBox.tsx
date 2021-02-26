d32f09421883c166ae470d5af82c076e214cdd26
Carlos <carlosaugustocf99@gmail.com>
2021-02-26 17:04:25 -0300

    Changed to activeChallenge

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,25 +1,26 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 
 export function ChallengeBox(){
-    const hasActiveChallenge = true;
-
-
+    const { activeChallenge, resetChallenge} = useContext(ChallengesContext);
 
     return(
         <div className={styles.challengeBoxContainer}>
-            { hasActiveChallenge ? (
+            { activeChallenge ? (
                <div className={styles.challengeActive}>
-                   <header>Ganhe 400xp</header>
+                   <header>Ganhe {activeChallenge.amount}</header>
                 <main>
-                    <img src='icons/body.svg' />
+                    <img src={`icons/${activeChallenge.type}.svg`} />
                     <strong>Novo desafio</strong>
-                    <p>Levante e faça uma caminhada</p>
+                    <p>{activeChallenge.description}</p>
                 </main>
                 <footer>
                     <button 
                     type='button'
-                    className={styles.challengeFailedButton}>
+                    className={styles.challengeFailedButton}
+                    onClick={resetChallenge}>
                         Falhei
                     </button>
                     <button 
@@ -40,7 +41,6 @@ export function ChallengeBox(){
                    </p>
                </div>
             )}
-         
         </div>
     )
 }
